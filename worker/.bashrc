be4632cf36e78114a759728ebe8ad12b8e2185af
Zoltan Mosonyi <zoltan@integritee.network>
2023-03-17 14:14:05 +0100

    fixed typos, removed commented code

--- worker/.bashrc
+++ worker/.bashrc
@@ -8,7 +8,7 @@ cat <<'MSG'
                        __/ |                    
                       |___/                     
 
-::::::::::::::::: Thanks for using Integritee's devevelpment environment! :::::::::::::::::
+::::::::::::::::: Thanks for using Integritee's development environment! :::::::::::::::::
 
 Please be sure you are following the documentation:
 https://docs.integritee.network/5-nodes-and-infrastructure/5.2-how-to-set-up-and-run-a-sidechain-or-tocw-node/5.2.3-software-requirements/5.2.3.2-docker
--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 MYUID="${MYUID:-1000}"
 MYGUID="${MYGUID:-1000}"
-#echo ${MYUID}
 
 u=`id -u ubuntu`
 if [ $u != $MYUID ] 
@@ -13,16 +12,5 @@ then
   groupmod -g ${MYGUID} ubuntu
   chown -R ubuntu.ubuntu /home/ubuntu
 fi
-#echo $MYUID
-#chown -R ubuntu.ubuntu /home/ubuntu 
-#echo $MYUID
-#echo ${@}
-# if [ ! -f /tmp/rustup.sh ]
-# then
-#     gosu ubuntu bash -c "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh"
-#     gosu ubuntu bash -c "chmod +x /tmp/rustup.sh"
-#     gosu ubuntu bash -c "/tmp/rustup.sh -y"
-# fi
-#exec gosu ubuntu '/home/ubuntu/.cargo/bin/rustup component add rust-src clippy rust-analysis rustfmt'
-#exec gosu ubuntu '/home/ubuntu/.cargo/bin/rustup self update'
+
 exec gosu ubuntu "${@}"
